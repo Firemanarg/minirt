2cfefd66be1f68cca8be92e1804677a23d236987
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-09-13 12:13:23 -0300

    Create matrix_inverse.c

---
+++ src/matrix/matrix_inverse.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   matrix_inverse.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 11:51:07 by marvin            #+#    #+#             */
+/*   Updated: 2023/09/13 11:51:07 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "matrix.h"
+
+t_matrix	*matrix_inverse(t_matrix *m)
+{
+	t_matrix	*inverse;
+	double		det;
+	int			row;
+	int			col;
+
+	det = matrix_determinant(m);
+	if (det == 0)
+		return (NULL);
+	inverse = matrix_new(m->rows, m->cols);
+	row = 0;
+	while (row < m->rows)
+	{
+		col = 0;
+		while (col < m->cols)
+		{
+			inverse->data[col][row] = matrix_cofactor(m, row, col) / det;
+			col += 1;
+		}
+		row += 1;
+	}
+	return (inverse);
+}
