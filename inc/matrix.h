72b58cfb849a329276fc56ffa3c47744a42e9761
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-09-16 22:44:27 -0300

    Remove old code & change struct names

--- inc/matrix.h
+++ inc/matrix.h
@@ -15,6 +15,7 @@
 
 # include <stdio.h>
 # include <math.h>
+# include <vec3.h>
 
 # define DEBUG_ON 0																// DEBUG
 
@@ -32,25 +33,6 @@ typedef struct s_matrix
 	double	**data;
 }			t_matrix;
 
-/* External { */
-typedef struct s_tuple
-{
-	double	x;
-	double	y;
-	double	z;
-	double	w;
-}			t_tuple;
-
-typedef t_tuple	t_point;
-typedef t_tuple	t_vector;
-/* } External */
-
-/* Temporary */
-t_tuple		*tuple_new(double x, double y, double z, double w);
-t_point		*point_new(double x, double y, double z);
-t_vector	*vector_new(double x, double y, double z);
-/* Temporary */
-
 t_matrix	*matrix_multiply(t_matrix *m, t_matrix *b);
 t_matrix	*matrix_new(int rows, int cols);
 t_matrix	*matrix_new_identity(int size);
@@ -59,8 +41,8 @@ void		matrix_print(t_matrix *m);
 t_matrix	*matrix_submatrix(t_matrix *m, int row, int col);
 t_matrix	*matrix_transpose(t_matrix *m);
 t_matrix	*matrix_inverse(t_matrix *m);
-t_matrix	*matrix_translation(t_vector *v);
-t_matrix	*matrix_scaling(t_vector *v);
+t_matrix	*matrix_translation(t_vec3 *v);
+t_matrix	*matrix_scaling(t_vec3 *v);
 t_matrix	*matrix_rotate_x(double rad);
 t_matrix	*matrix_rotate_y(double rad);
 t_matrix	*matrix_rotate_z(double rad);
@@ -68,6 +50,6 @@ double		matrix_determinant(t_matrix *m);
 double		matrix_determinant_2x2(t_matrix *m);
 double		matrix_minor(t_matrix *m, int row, int col);
 double		matrix_cofactor(t_matrix *m, int row, int col);
-// t_matrix	*mtx_shear(t_matrix *m, t_vector x, t_vector y);
+// t_matrix	*mtx_shear(t_matrix *m, t_vec3 x, t_vec3 y);
 
 #endif
