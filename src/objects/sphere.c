6d4a342646beb77cbbddb7ac97d75f30df162872
gmachado <gmachado@student.42sp.org.br>
2023-09-18 11:33:19 -0300

    feat: Calculate square root of the discriminant once.

--- src/objects/sphere.c
+++ src/objects/sphere.c
@@ -6,7 +6,7 @@
 /*   By: gmachado <gmachado@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 19:11:05 by gmachado          #+#    #+#             */
-/*   Updated: 2023/09/18 03:57:07 by gmachado         ###   ########.fr       */
+/*   Updated: 2023/09/18 11:28:11 by gmachado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_err	sphere_intersect(t_obj *sphere, t_ray *ray, t_varray *r)
 	double	a;
 	double	b;
 	double	c;
-	double	disc;
+	double	sqrt_disc;
 	t_vec3	vecs[2];
 
 	set_vec3(0.0, 0.0, 0.0, vecs);
@@ -27,9 +27,10 @@ t_err	sphere_intersect(t_obj *sphere, t_ray *ray, t_varray *r)
 	a = dot(ray->direction, ray->direction);
 	b = 2 * dot(ray->direction, vecs + 1);
 	c = dot(vecs + 1, vecs + 1) - 1;
-	disc = b * b - 4 * a * c;
-	if (disc < 0)
+	sqrt_disc = b * b - 4 * a * c;
+	if (sqrt_disc < 0)
 		return (OK);
-	return (insert_into_array(r, (-b - sqrt(disc)) / (2.0 * a), sphere)
-		|| insert_into_array(r, (-b + sqrt(disc)) / (2.0 * a), sphere));
+	sqrt_disc = sqrt(sqrt_disc);
+	return (insert_into_array(r, (-b - sqrt_disc) / (2.0 * a), sphere)
+		|| insert_into_array(r, (-b + sqrt_disc) / (2.0 * a), sphere));
 }
