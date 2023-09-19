5ef5f0a5567603db035e7afc18039d9189849dd8
gmachado <gmachado@student.42sp.org.br>
2023-09-19 14:45:43 -0300

    feat: Separate matrix-vector and matrix-point multiplication function. Vectors should not respond to translate transformations.

--- src/matrix/matrix_vec3_multiply.c
+++ src/matrix/matrix_vec3_multiply.c
@@ -6,14 +6,24 @@
 /*   By: gmachado <gmachado@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 01:28:25 by gmachado          #+#    #+#             */
-/*   Updated: 2023/09/19 02:22:43 by gmachado         ###   ########.fr       */
+/*   Updated: 2023/09/19 10:49:34 by gmachado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <matrix.h>
 #include <vec3.h>
 
-void	matrix_vec3_multiply(t_matrix *matrix, t_vec3 *v, t_vec3 *result)
+void	matrix_vector_multiply(t_matrix *matrix, t_vec3 *v, t_vec3 *result)
+{
+	double	**m;
+
+	m = matrix->data;
+	result->x = m[0][0] * v->x + m[0][1] * v->y + m[0][2] * v->z;
+	result->y = m[1][0] * v->x + m[1][1] * v->y + m[1][2] * v->z;
+	result->z = m[2][0] * v->x + m[2][1] * v->y + m[2][2] * v->z;
+}
+
+void	matrix_point_multiply(t_matrix *matrix, t_vec3 *v, t_vec3 *result)
 {
 	double	**m;
 
