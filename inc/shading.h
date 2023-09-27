7e96a2b15cef6a5b88eab65736e9034dd9338523
gmachado <gmachado@student.42sp.org.br>
2023-09-26 21:09:46 -0300

    fix: Apply fix to leak in matrix_apply().

--- inc/shading.h
+++ inc/shading.h
@@ -6,13 +6,31 @@
 /*   By: gmachado <gmachado@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 15:11:53 by lsilva-q          #+#    #+#             */
-/*   Updated: 2023/10/03 03:33:27 by gmachado         ###   ########.fr       */
+/*   Updated: 2023/10/03 03:37:41 by gmachado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHADING_H
 # define SHADING_H
 
+# include <vec3.h>
+
+typedef t_vec3	t_color;
+
+typedef struct s_point_light
+{
+	t_color	intensity;
+	t_vec3	position;
+}	t_point_light;
+
+typedef struct s_material
+{
+	double	ambient;
+	double	diffuse;
+	double	specular;
+	double	shininess;
+	t_color	color;
+}	t_material;
 # include "objects.h"
 
 typedef struct s_phong_params
