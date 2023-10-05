48c8d6c9496f2b5e7aa73e5fbd01614be843a5c4
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-10-05 15:42:26 -0300

    Add macros

--- inc/parser.h
+++ inc/parser.h
@@ -17,9 +17,20 @@
 # include "error.h"
 # include "vec3.h"
 
+# define MAX_RATIO 1.0
+# define MIN_RATIO 0.0
+# define MAX_FOV 180.0
+# define MIN_FOV 0.0
+# define AMBIENT_LIGHT_FIELDS_COUNT 3
+# define CAMERA_FIELDS_COUNT 4
+# define LIGHT_FIELDS_COUNT 4
+# define SPHERE_FIELDS_COUNT 4
+# define CYLINDER_FIELDS_COUNT 6
+# define PLANE_FIELDS_COUNT 4
+
+// parser.c
 t_scene		*parse_file(char *file_name);
 t_base_obj	*parse_line(char *line);
-
 t_obj_type	get_type_by_str(char *str);
 
 t_base_obj	*parse_ambient_light(char **fields, int fields_count);
