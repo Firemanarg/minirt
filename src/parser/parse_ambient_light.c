b833970536ca0879511bcbaaa85a4a6bd21cc3cc
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-10-06 09:52:56 -0300

    Implement parser for ambient_light

--- src/parser/parse_ambient_light.c
+++ src/parser/parse_ambient_light.c
@@ -12,17 +12,34 @@
 
 #include "parser.h"
 
-t_base_obj	*parse_ambient_light(char **fields, int fields_count)
+static int	is_valid(t_ambient_light *light);
+
+t_ambient_light	*parse_ambient_light(char **fields, int fields_count)
 {
 	t_ambient_light	*light;
+	t_err			err;
 
 	if (fields_count != AMBIENT_LIGHT_FIELDS_COUNT)
 		return (NULL);
 	light = malloc(sizeof(t_ambient_light));
 	if (!light)
 		return (NULL);
-	light->base.type = AMBIENT_LIGHT;
-	light->base.color = parse_color(fields[1]);
-	light->base.brightness = parse_double(fields[2]);
-	return ((t_base_obj *)light);
+	light->type = AMBIENT_LIGHT;
+	light->ratio = ft_atod(fields[1]);
+	err = str_to_vec3(fields[2], &light->color);
+	if (err != OK || !is_valid(light))
+	{
+		free(light);
+		return (NULL);
+	}
+	return (light);
+}
+
+static int	is_valid(t_ambient_light *light)
+{
+	if (!is_valid_color(&light->color))
+		return (0);
+	if (!is_valid_ratio(light->ratio))
+		return (0);
+	return (1);
 }
