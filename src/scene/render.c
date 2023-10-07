76f39a6146bc8c8f67166a5ee19309bd39d455dc
gmachado <gmachado@student.42sp.org.br>
2023-10-07 01:14:18 -0300

    refactor: Precompute origin of ray cast from camera.

--- src/scene/render.c
+++ src/scene/render.c
@@ -6,27 +6,32 @@
 /*   By: gmachado <gmachado@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 12:33:27 by gmachado          #+#    #+#             */
-/*   Updated: 2023/10/07 00:14:49 by gmachado         ###   ########.fr       */
+/*   Updated: 2023/10/07 01:13:09 by gmachado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shading.h"
 #include "graphics.h"
 
-void	ray_for_pixel(t_camera *camera, int p_x, int p_y, t_ray *ray)
+static void	get_transformed_ray_origin(t_camera *camera, t_ray *ray)
 {
 	t_vec3	zero;
+
+	zero.x = 0.0;
+	zero.y = 0.0;
+	zero.z = 0.0;
+	matrix_point_multiply(camera->inv_transform, &zero, &ray->start);
+}
+
+void	ray_for_pixel(t_camera *camera, int p_x, int p_y, t_ray *ray)
+{
 	t_vec3	world;
 	t_vec3	pixel;
 
 	world.x = camera->half_width - (p_x + 0.5) * camera->pixel_size;
 	world.y = camera->half_height - (p_y + 0.5) * camera->pixel_size;
 	world.z = -1;
-	zero.x = 0.0;
-	zero.y = 0.0;
-	zero.z = 0.0;
 	matrix_point_multiply(camera->inv_transform, &world, &pixel);
-	matrix_point_multiply(camera->inv_transform, &zero, &ray->start);
 	subtract(&pixel, &ray->start, &ray->direction);
 	normalize(&ray->direction, &ray->direction);
 }
@@ -42,6 +47,7 @@ t_err	render_image(t_camera *camera, t_scene *world, t_mlx_data *mlx_data)
 	xs = new_array(INITIAL_ARRAY_SIZE);
 	if (!xs)
 		return (ERR_ALLOC);
+	get_transformed_ray_origin(camera, &ray);
 	y = 0;
 	while (y < camera->vsize)
 	{
