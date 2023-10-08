738ab80b9043ab99898589ac82dd3258e1e9d98b
gmachado <gmachado@student.42sp.org.br>
2023-10-07 23:32:43 -0300

    fix: Move static function declaration to fix compiler error.

--- src/vec3/vec3.c
+++ src/vec3/vec3.c
@@ -6,7 +6,7 @@
 /*   By: gmachado <gmachado@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 01:37:49 by gmachado          #+#    #+#             */
-/*   Updated: 2023/10/07 18:53:47 by gmachado         ###   ########.fr       */
+/*   Updated: 2023/10/07 23:27:17 by gmachado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,6 @@ void	set_vec3(double x, double y, double z, t_vec3 *t)
 	t->x = x;
 	t->y = y;
 	t->z = z;
-}
-
-/**
- * @brief Converts a string to a t_vec3.
- *
- * @param str The string to be converted.
- * @param vec The t_vec3 to store the converted string.
- *
- * @return t_err Returns OK if the conversion was successful, INVALID_VEC3 if
- * the string is not a valid t_vec3, or ERR_ALLOC if there was an allocation
- * error.
-*/
-t_err	str_to_vec3(const char *str, t_vec3 *vec)
-{
-	char	**arr;
-
-	if (!is_str_vec3_valid(str))
-		return (INVALID_VEC3);
-	arr = ft_split(str, ',');
-	if (arr == NULL)
-		return (ERR_ALLOC);
-	vec->x = ft_atod(arr[0]);
-	vec->y = ft_atod(arr[1]);
-	vec->z = ft_atod(arr[2]);
-	ft_clear_strarr(arr);
-	return (OK);
 }
 
 static int	is_str_vec3_valid(const char *str)
@@ -70,4 +44,30 @@ static int	is_str_vec3_valid(const char *str)
 	if (comma_count != 2)
 		return (0);
 	return (1);
+}
+
+/**
+ * @brief Converts a string to a t_vec3.
+ *
+ * @param str The string to be converted.
+ * @param vec The t_vec3 to store the converted string.
+ *
+ * @return t_err Returns OK if the conversion was successful, INVALID_VEC3 if
+ * the string is not a valid t_vec3, or ERR_ALLOC if there was an allocation
+ * error.
+*/
+t_err	str_to_vec3(const char *str, t_vec3 *vec)
+{
+	char	**arr;
+
+	if (!is_str_vec3_valid(str))
+		return (INVALID_VEC3);
+	arr = ft_split(str, ',');
+	if (arr == NULL)
+		return (ERR_ALLOC);
+	vec->x = ft_atod(arr[0]);
+	vec->y = ft_atod(arr[1]);
+	vec->z = ft_atod(arr[2]);
+	ft_clear_strarr(arr);
+	return (OK);
 }
