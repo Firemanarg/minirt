8bdaaf20fc1428ed5881569503845970b4abf8fd
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-10-15 05:11:59 -0300

    shading.h: Use camera from scene

--- inc/shading.h
+++ inc/shading.h
@@ -26,8 +26,7 @@ void		reflect(t_vec3 *incident, t_vec3 *normal, t_vec3 *reflected);
 // render.c
 void		get_transformed_ray_origin(t_camera *camera, t_ray *ray);
 void		ray_for_pixel(t_camera *camera, int p_x, int p_y, t_ray *ray);
-t_err		render_image(t_camera *camera, t_scene *world,
-				t_mlx_data *mlx_data);
+t_err		render_image(t_scene *world, t_mlx_data *mlx_data);
 
 // scene.c
 t_err		intersect_world(t_scene *w, t_ray *r, t_varray *xs);
