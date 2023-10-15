670149e7b49b6fac5bb9cbcc972f3a42360b2241
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-10-15 05:12:36 -0300

    minirt.c: Implement main function

--- src/minirt.c
+++ src/minirt.c
@@ -10,14 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <minirt.h>
-#include <graphics.h>
+#include "minirt.h"
+#include "graphics.h"
+#include "parser.h"
+#include "error.h"
+#include "shading.h"
 
-int	main(void)
+static void	init(t_args *args, char *file_name);
+
+int	main(int argc, char **argv)
 {
 	t_args	args;
+	t_err	err;
 
+	if (argc != 2)
+	{
+		printf("Error\nUsage: ./miniRT <file.rt>\n");
+		return (1);
+	}
+	init(&args, argv[1]);
+	err = render_image(args.scene, &args.mlx_data);
+	if (err != OK)
+	{
+		printf("Error during render\n");
+		return (1);
+	}
+	free_scene(args.scene);
 	create_window(&args);
+	// scene = parse_file(argv[1]);
+	// if (scene == NULL)
+	// 	return (1);
+	// init_args(&args, width, height);
+	// render_image(&world.camera, &world, &args.mlx_data);
+	// create_window(&args);
 	return (0);
-	return (0);
+}
+
+static void	init(t_args *args, char *file_name)
+{
+	args->scene = parse_file(file_name);
+	if (args->scene == NULL)
+	{
+		printf("Error parsing file\n");
+		exit(1);
+	}
+	init_args(args);
 }
