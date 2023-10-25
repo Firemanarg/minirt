a39243afe6a74d6d9b4568523ce397e958397180
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-10-25 11:32:47 -0300

    comments: Remove comments

--- src/parser/parse_ambient_light.c
+++ src/parser/parse_ambient_light.c
@@ -67,35 +67,3 @@ static t_err	validate_fields(t_parser_obj *obj)
 		obj->status = INVALID_RATIO;
 	return (obj->status);
 }
-
-// static int	is_valid(t_ambient_light *light);
-
-// t_err	parse_ambient_light(char **fields, int fields_count,
-// 			t_ambient_light *light)
-// {
-// 	t_err	err;
-
-// 	if (fields_count != AMBIENT_LIGHT_FIELDS_COUNT)
-// 		return (INVALID_ARG);
-// 	if (light == NULL)
-// 		return (INVALID_ARG);
-// 	light->type = AMBIENT_LIGHT;
-// 	light->ratio = ft_atod(fields[1]);
-// 	err = str_to_vec3(fields[2], &light->color);
-// 	err |= !is_valid(light);
-// 	if (err != OK)
-// 		return (err);
-// 	light->color.r = light->color.r * light->ratio / 255.0;
-// 	light->color.g = light->color.g * light->ratio / 255.0;
-// 	light->color.b = light->color.b * light->ratio / 255.0;
-// 	return (OK);
-// }
-
-// static int	is_valid(t_ambient_light *light)
-// {
-// 	if (!is_valid_color(&light->color))
-// 		return (0);
-// 	if (!is_valid_ratio(light->ratio))
-// 		return (0);
-// 	return (1);
-// }
