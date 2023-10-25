d7721319de82c10ae831ad471bd34da34f5e3c99
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-10-25 16:03:08 -0300

    parser: Fix memory warnings

--- src/parser/parser_2.c
+++ src/parser/parser_2.c
@@ -6,14 +6,14 @@
 /*   By: lsilva-q <lsilva-q@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 15:04:39 by lsilva-q          #+#    #+#             */
-/*   Updated: 2023/10/23 16:28:14 by lsilva-q         ###   ########.fr       */
+/*   Updated: 2023/10/25 15:56:48 by lsilva-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-static void	*iter_print_errors(void *content, size_t, int, int);
-static void	*add_obj_to_scene(void *content, size_t, int, int);
+static void	*iter_print_errors(void *content, size_t i, int f, int l);
+static void	*add_obj_to_scene(void *content, size_t i, int f, int l);
 
 void	assign_parser_to_objs(t_scene_parser *parser)
 {
@@ -43,10 +43,11 @@ int	print_parser_errors(t_scene_parser *parser)
 	return (parser->err_flag);
 }
 
-static void	*iter_print_errors(void *content, size_t, int, int)
+static void	*iter_print_errors(void *content, size_t i, int f, int l)
 {
 	t_parser_obj	*obj;
 
+	(void) (i && f && l);
 	obj = (t_parser_obj *) content;
 	if (obj->status != OK)
 	{
@@ -59,17 +60,22 @@ static void	*iter_print_errors(void *content, size_t, int, int)
 void	populate_scene(t_scene_parser *parser)
 {
 	parser->scene = malloc(sizeof(t_scene));
-	parser->scene->geometries = malloc(sizeof(t_geom_obj));
-	parser->scene->lights = malloc(sizeof(t_point_light));
+	parser->scene->geometries = ft_calloc(
+			parser->geometry_count + 1, sizeof(t_geom_obj));
+	parser->scene->lights = ft_calloc(
+			parser->light_count + 1, sizeof(t_point_light));
 	ft_lst_func_apply(&parser->objs, add_obj_to_scene);
+	parser->scene->geometries[parser->geometry_count] = (t_geom_obj){0};
+	parser->scene->lights[parser->light_count] = (t_point_light){0};
 }
 
-static void	*add_obj_to_scene(void *content, size_t, int, int)
+static void	*add_obj_to_scene(void *content, size_t i, int f, int l)
 {
 	t_scene_parser	*parser;
 	t_parser_obj	*obj;
 	t_scene			*scene;
 
+	(void) (i && f && l);
 	obj = (t_parser_obj *) content;
 	parser = obj->parser;
 	scene = parser->scene;
@@ -82,8 +88,8 @@ static void	*add_obj_to_scene(void *content, size_t, int, int)
 		scene->lights[parser->light_index] = *((t_point_light *) obj->obj);
 		parser->light_index += 1;
 	}
-	else if (obj->type == SPHERE || obj->type == CYLINDER ||
-			obj->type == PLANE)
+	else if (obj->type == SPHERE || obj->type == CYLINDER
+		|| obj->type == PLANE)
 	{
 		scene->geometries[parser->geometry_index] = *((t_geom_obj *) obj->obj);
 		parser->geometry_index += 1;
